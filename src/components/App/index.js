b17d1c5947918d12de15b4a9c5d28e71a1d94943
Nikolay Chikovani <nchikovany1@gmail.com>
2020-06-03 21:58:38 +0300

    init, setup finished

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+// import '../Header'
+
+function App() {
+  return (
+    <div className="page">
+      <header className="header"/>
+      <div className="page-content">
+        ПРЕВЭЭЭТ!!1
+      </div>
+      <footer className="footer"/>
+    </div>
+  );
+}
+
+export default App;
